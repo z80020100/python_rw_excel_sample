1fd2cc51583bf88a174cf4d24acf3014feacde9e
Cliff Wu <z800201002005@gmail.com>
2023-05-25 16:23:57 +0900

    Add program to demo reading a specific range of values in Excel

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from workbook import Workbook
 TARGET_EXCEL_FILE = "sample.xlsm"
 TARGET_EXCEL_SHEET = "Sheet1"
 TARGET_EXCEL_CELL = "AZ3"
+TARGET_EXCEL_RANGE = "A1:A3"
 
 
 def main():
@@ -14,6 +15,11 @@ def main():
     # Read cell
     AZ3 = wb.get_cell(TARGET_EXCEL_SHEET, TARGET_EXCEL_CELL)
     print(AZ3)
+    # Read range
+    range = wb.get_range(TARGET_EXCEL_SHEET, TARGET_EXCEL_RANGE)
+    # Convert range to list
+    column_A = range.values.T[0]
+    print(column_A)
 
 
 if __name__ == "__main__":
