886fad4dcc20213b68808ca55934d3618241b065
Cliff Wu <z800201002005@gmail.com>
2023-05-26 19:12:16 +0900

    Add program to demo writing value to a cell or range

--- main.py
+++ main.py
@@ -5,23 +5,35 @@ from workbook import Workbook
 
 TARGET_EXCEL_FILE = "sample.xlsm"
 TARGET_EXCEL_SHEET = "Sheet1"
-TARGET_EXCEL_CELL = "AZ3"
-TARGET_EXCEL_RANGE = "A1:A3"
+TARGET_READ_EXCEL_CELL = "AZ3"
+TARGET_READ_EXCEL_RANGE = "A1:A3"
+TARGET_WRITE_EXCEL_CELL = "A4"
+TARGET_WRITE_EXCEL_RANGE = "A5:A7"
 
 
 def main():
+    # Sample for reading
     wb = Workbook(TARGET_EXCEL_FILE)
     sheets = wb.get_sheets()
     # Get sheets
     print(sheets)
     # Read cell
-    AZ3 = wb.get_cell(TARGET_EXCEL_SHEET, TARGET_EXCEL_CELL)
+    AZ3 = wb.get_cell(TARGET_EXCEL_SHEET, TARGET_READ_EXCEL_CELL)
     print(AZ3)
     # Read range
-    range = wb.get_range(TARGET_EXCEL_SHEET, TARGET_EXCEL_RANGE)
+    range = wb.get_range(TARGET_EXCEL_SHEET, TARGET_READ_EXCEL_RANGE)
     # Convert range to list
     column_A = range.T[0]
     print(column_A)
+
+    # Sample for writing
+    wb = Workbook(TARGET_EXCEL_FILE, writable=True)
+    # Write cell
+    wb.set_cell(TARGET_EXCEL_SHEET, TARGET_WRITE_EXCEL_CELL, 100)
+    # Write range
+    wb.set_range(TARGET_EXCEL_SHEET, TARGET_WRITE_EXCEL_RANGE, 1000)
+    # Save workbook
+    wb.save()
 
 
 if __name__ == "__main__":
